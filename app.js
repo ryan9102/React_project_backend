30f49172fd741bc2df19c2045e558b7b87a8a42b
Hualiang <ryan_li@outlook.com.au>
2022-04-02 13:06:10 +1100

    add error model

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const placesRoutes = require("./routes/places-routes");
+
 const app = express();
+
+app.use("/api/places", placesRoutes);
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message } || "An unknown error occured.");
+});
+//if a midware function has four parameters, it will be recognized as the default error handling middleware
 
 app.listen(5000);
