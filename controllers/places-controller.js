7d0226c1f1d73c147388082303b0b9b49bd361a6
Hualiang <ryan_li@outlook.com.au>
2022-04-17 16:26:53 +1000

    add authorization

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -134,6 +134,12 @@ const updatePlace = async (req, res, next) => {
     return next(error);
   }
 
+
+  if (place.creator.toString() !== req.userData.userId) {
+    const error = new HttpError("You're not allowed to edit this place.", 401);
+    return next(error);
+  }
+
   place.title = title;
   place.description = description;
 
